6f1073a224351fe5c442206f6229640dbe91c923
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-03-12 13:17:58 +0200

    filters

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,12 +1,27 @@
 import { Task } from "../Task/Task";
 import css from "./TaskList.module.css";
+import { useSelector } from "react-redux";
+import { statusFilters } from "../../redux/constants";
+
+const getVisibleTasks = (tasks, statusFilters) => {
+  switch (statusFilters) {
+    case statusFilters.active:
+      return tasks.filter((task) => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter((task) => task.completed);
+    default:
+      return tasks;
+  }
+};
 
 export const TaskList = () => {
-  const tasks = [];
+  const tasks = useSelector((state) => state.tasks);
+
+  const visibleTasks = getVisibleTasks(tasks, statusFilters);
 
   return (
     <ul className={css.list}>
-      {tasks.map((task) => (
+      {visibleTasks.map((task) => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
