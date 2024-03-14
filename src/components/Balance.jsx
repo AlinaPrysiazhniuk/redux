c317b3acdb2164313eb2a695ed70e19b00477327
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-03-14 14:03:51 +0200

    delete

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -3,20 +3,11 @@ import { deposit, withdraw } from "../redux/balanceSlice";
 import { useState } from "react";
 
 export default function Balance() {
-  //компонент потрібно підписати на state, для цього використовуємо useSelector
-  //useSelector викликаємо в будь-якому місці компоненту, де потрібно отримати доступ до
-  //шматочку стану, передали туди анонімну функцію, вона отримує один апарметр
-  //це весь стан редаксу і повертаєте той шматочок який вам потрібен
-  //те, що вона повернула  - то результат useSelector, записали цей
-  //результат в змінну і викликали де потрібно
   const balance = useSelector((state) => state.balance.value);
 
-  // для того щоб відправити action є dispatch
-  //викликаємо  useDispatch(), туди передаємо потрібний action,
-  // що буде відправлено до store при певній події
   const dispatch = useDispatch();
 
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState("");
 
   return (
     <div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
-// import { composeWithDevTools } from "@redux-devtools/extension";
 import { balanceReducer } from "./balanceSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
@@ -7,52 +6,6 @@ const rootReducer = combineReducers({
   balance: balanceReducer,
 });
 
-//оголошуємо стор - 1 крок
 export const store = configureStore({
   reducer: rootReducer,
 });
-
-// const initialState = {
-//   balance: {
-//     value: 100,
-//   },
-//   locale: {
-//     lang: "uk",
-//   },
-// };
-
-//reducer викликається кожного разу як реакція на відправку action
-
-//Reducer обробляє екшени
-// const rootReducer = (state = initialState, action) => {
-//   switch (
-//     action.type //switch за типом action вибирає потрібний case для відпрацювання) {
-//   ) {
-//     case "balance/deposit":
-//       return {
-//         ...state,
-//         balance: {
-//           ...state.balance,
-//           value: state.balance.value + action.payload,
-//         },
-//       };
-//     case "balance/withdraw":
-//       return {
-//         ...state,
-//         balance: {
-//           ...state.balance,
-//           value: state.balance.value - action.payload,
-//         },
-//       };
-//     case "locale/lang":
-//       return {
-//         ...state,
-//         locale: {
-//           ...state.locale,
-//           lang: action.payload,
-//         },
-//       };
-//     default:
-//       return state;
-//   }
-// };
